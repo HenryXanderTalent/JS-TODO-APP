c2bd1168672af7ea635b560be5e7c2f3320a24fd
HenryXanderTalent <henry.opara@xandertalent.com>
2023-04-01 17:37:26 +0100

    added display task and toggle completed button

--- todo-app-main/script.js
+++ todo-app-main/script.js
@@ -1,3 +1,7 @@
+//data object
+
+let data = []
+
 const tasksContainer = document.getElementById("dom-space-div");
 const taskInput = document.querySelector("#input-task"); 
 const taskQty = document.getElementById("items-num");
@@ -36,40 +40,119 @@ function output(task){
     num++
     qty++
 
-    
+    data.push({
+        id: `${num}`,
+        task: `${task}`,
+        progress: true,
+        deleted: false
+    })
 
-    let newcard = document.createElement('div')
-    let newElementRadio = document.createElement('input')
-    let newElementInput = document.createElement('span')
-    let newElementcross = document.createElement('button')
-  
-    //console.log(newElementCross);
+    console.log(data)
 
-    tasksContainer.appendChild(newcard).classList.add('task-card')
-    newcard.appendChild(newElementRadio).setAttribute('type', 'radio')
-    newcard.appendChild(newElementInput).textContent = task
-    newcard.appendChild(newElementcross).setAttribute('id', 'bt')
-    //.classList.add('card-btn')
+    addDiv(num, task)
+    //console.log(num);
+    //console.log(qty)
 
-    tasksContainer.lastChild.setAttribute('id', `${num}`)
-    console.log(num);
-    console.log(qty)
-
-    document.getElementById(`${num}`).addEventListener('click', function() {
-        //console.log(this.parentNode);
-        //let removeCard = this.parentNode
-        
-        newcard.remove(this)
+    let prs = document.getElementById(`${num}`)
+    //console.log(prs.lastChild)
+    prs.lastChild.addEventListener('click', function(){
+      
+        tasksContainer.removeChild(this.parentNode)
         qty--
         taskQty.innerText = qty
+    
+        //console.log(this.getAttribute('id'))
+        deleteTask(this.parentNode.getAttribute('id'))
+    
+        console.log(data)
+
+    })
+
+    let rdo = document.getElementById(`${num}`)
+    //console.log(rdo.firstChild)
+    rdo.firstChild.addEventListener('click', function(){
+
+        //console.log(rdo.getAttribute('id'))
+
+        let rdoID = rdo.getAttribute('id')
+
+        for(let i = 0; i < data.length; i++){
+            if(data[i].id === rdoID) {
+                data[i].progress = false
+                break;
+            }
+        }
+
+        console.log(data)
+
+        //filter through objects to find it and set progress to false
     })
 
     taskQty.innerText = qty
 
 }
 
+function deleteTask(id) {
+     
+    for(let i = 0; i < data.length; i++){
+        if(data[i].id === id) {
+            data.splice(i, 1)
+            break;
+        }
+    }
 
+    //displayTasks()
 
+    /*let dataFiltered = data
+
+    data = dataFiltered.filter(function(t) {
+        return t.id !== id
+    })*/
+ 
+}
+
+function addDiv(num, task) {
+
+        let newcard = document.createElement('div')
+        let newElementRadio = document.createElement('input')
+        let newElementInput = document.createElement('span')
+        let newElementcross = document.createElement('button')
+      
+        //console.log(newElementCross);
+    
+        tasksContainer.appendChild(newcard).classList.add('task-card')
+        newcard.appendChild(newElementRadio).setAttribute('type', 'radio')
+        newcard.appendChild(newElementInput).textContent = task
+        newcard.appendChild(newElementcross).setAttribute('class', 'bt')
+        //.classList.add('card-btn')
+
+        displayTasks()
+    
+}
+
+function displayTasks() {
+
+    //for each item in arr display it
+    for(let i = 0; i < data.length; i++) {
+
+        tasksContainer.lastChild.setAttribute('id', num)
+
+    }
+}
+
+/*document.getElementById(`${num}`).addEventListener('click', function(){
+      //console.log(this.parentNode);
+    //let removeCard = this.parentNode   
+    
+    newcard.remove(this)
+    qty--
+    taskQty.innerText = qty
+
+    //console.log(this.getAttribute('id'))
+    deleteTask(this.getAttribute('id'))
+
+    console.log(data)
+}*/
 
 
 
